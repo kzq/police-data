b9df008b5fc792f821d000307c9d9333119a1bc0
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-09 23:30:59 +0000

    Configure cucumber env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,9 @@
 
 require 'cucumber/rails'
 require 'capybara'
+require 'capybara/poltergeist'
+require 'capybara-screenshot'
+require 'factory_girl_rails'
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
@@ -36,6 +39,10 @@ rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
+AfterConfiguration do
+  DatabaseCleaner.clean
+end
+
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
 #
@@ -56,3 +63,5 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 20
