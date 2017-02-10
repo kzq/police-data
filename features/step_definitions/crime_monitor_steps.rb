8400a58bf15e3c7b9be909dd13831d69d08475c2
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-10 14:27:52 +0000

    Add steps for search crimes based on given post code

---
+++ features/step_definitions/crime_monitor_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am on the home page$/) do
+  visit root_path
+end
+
+When(/^I provide my postcode (.*)$/) do |post_code|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |text_heading|
+  pending # Write code here that turns the phrase above into concrete actions
+end
