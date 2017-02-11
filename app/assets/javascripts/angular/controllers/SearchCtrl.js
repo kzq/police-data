b217ce5d4dd989698c6abcca778f3cf5330f13eb
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-11 14:49:07 +0000

    Get request for crime data

--- app/assets/javascripts/angular/controllers/SearchCtrl.js
+++ app/assets/javascripts/angular/controllers/SearchCtrl.js
@@ -1,3 +1,21 @@
-app.controller('SearchCtrl',function($scope){
-    
-});
+app.controller('SearchCtrl', ['$scope', 'PoliceDataFactory', function($scope, PoliceDataFactory){
+  $scope.search = {};
+  
+  $scope.performSearch = function() {
+    $scope.search.error = '';
+    var geocoder = new Geocoder($scope.search.postcode);
+    geocoder.geocode($scope.queryCrime);
+  };  
+  
+  $scope.queryCrime = function(geocoding){
+    $scope.response = PoliceDataFactory.query({path: 'crimes-street/all-crime', query: 'lat='+geocoding.lat+'&lng='+geocoding.lng});
+    if (geocoding.error == false){
+      $scope.search.postcode = '';
+    }
+    else if (geocoding.error == true){
+      $scope.search.error = geocoding.errorText;
+    }   
+  };
+  $scope.crimes = {}; 
+
+}]);
