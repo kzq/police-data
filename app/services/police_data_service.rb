b23ba0105f3a4b05848d4fd0057081a07740a7bd
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-13 17:31:54 +0000

    Perform calculations on police data
    Data is returned in json array
    Should be converted into hash
    and statistics are get extrated

--- app/services/police_data_service.rb
+++ app/services/police_data_service.rb
@@ -1,3 +1,23 @@
 class PoliceDataService
+  def street_level_crimes_data(json_data)
+    crimes_hash=to_hash(json_data)
+    # manipulate hash data
+    # exp: {antisocial: 221, violent: 35 }
+    total_crimes_per_categaory = crimes_hash.each_with_object(Hash.new(0)) { |h1, h2| h2[h1["category"]] += 1 }
+    # sort by number of crimes in desending order
+    total_crimes_per_categaory = total_crimes_per_categaory.sort_by {|_key, value| value}.reverse.to_h
+    # total crimes 
+    total_crimes_committed = crimes_hash.count
+    # convert hash to array of hashes
+    # exp: [{category: "antisocial", total: 221},{category: "violent", total: 35}]
+    statistics = total_crimes_per_categaory.collect {|key,value| {category: key,total: value}}
+    # return array of hashes
+    hash = {total: total_crimes_committed, statistics: statistics}
+    hash_array = []
+    hash_array.push(hash)
+  end
   
+  def to_hash(json)
+    JSON.parse(json)
+  end
 end
--- spec/services/police_data_service_spec.rb
+++ spec/services/police_data_service_spec.rb
@@ -1,3 +1,18 @@
 describe PoliceDataService do
+  let(:data) {[{'category'=> 'anti-social-behaviour', 'persistent_id'=> '', 'location_type'=> 'Force', 'location_subtype'=> '','id'=> 20599642,'location'=> {'latitude'=> '52.6269479','longitude'=> '-1.1121716'}},{'category'=> 'burglary','persistent_id'=> 'aebd220e869a235ba92cde43f7e0df29001573b3df1b094bb952820b2b8f44b0','location_type'=> 'Force','location_subtype'=> '','id'=> 20604632,'location'=> {'latitude'=> '52.6271606','longitude'=> -1.1485111}}].to_json}
+  let(:police_data_service) {described_class.new}
+  
+  describe "#street_level_crimes_data" do
+    it "performs calcultions on given data and return statistics" do
+      stats_extracted_from_data = [{:total=>2,:statistics=>[{:category=>"burglary", :total=>1},{:category=>"anti-social-behaviour", :total=>1}]}]
+      expect(police_data_service.street_level_crimes_data(data)).to eq(stats_extracted_from_data)   
+    end
+  end
+  
+  describe "#to_hash" do
+    it "converts given json to a hash" do
+      expect(police_data_service.to_hash(data)).to be_a(Array || Hash) 
+    end
+  end
   
 end
